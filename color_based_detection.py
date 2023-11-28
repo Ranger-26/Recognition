1673427f52e451d29d9d340afd371731116816e2
Siddhu Mohan <siddhu.mohan6@gmail.com>
2023-11-28 17:04:06 -0600

    Print out amount of color

--- color_based_detection.py
+++ color_based_detection.py
@@ -21,11 +21,10 @@ class Color:
         return frame_filter_2
 
     def get_color_percentage(self, image: np.array) -> float:
-        image_filtered = self.filter_image(image)
         color_mask = cv2.inRange(frame_hsv, self.lower_bound, self.upper_bound)
 
                 # Calculate the total number of pixels in the image
-        total_pixels = image_filtered.size // image_filtered.itemsize
+        total_pixels = image.size // image.itemsize
 
         # Calculate the number of pixels within the specified color range
         color_pixels = np.sum(color_mask > 0)
@@ -38,7 +37,7 @@ class Color:
 
 color_blue:Color = Color([106,50,50],[118,255,255])
 color_red:Color = Color([0,100,100], [1,255,255])
-
+color_green:Color = Color([51,0,150],[64,100, 255])
 frame_hsv: np.array = None
 
 def color_search() -> None:
@@ -51,8 +50,8 @@ def color_search() -> None:
             print("The mouse was clicked at x= ", x, "y = ", y)
             print("Hue = ", hsv[0],  "Sat = ", hsv[1], "Val = ", hsv[2])
 
-    cv2.namedWindow("Color finder")
-    cv2.setMouseCallback("Color finder", mouseClick, param = None)
+    cv2.namedWindow("Main")
+    cv2.setMouseCallback("Main", mouseClick, param = None)
 
 
     # Open the default camera (you can change the parameter to use a different camera)
@@ -69,9 +68,18 @@ def color_search() -> None:
         frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
         # Display the frame
-        cv2.imshow('Color finder', frame)
         cv2.imshow('Color isolation red', color_red.filter_image(frame))
         cv2.imshow('Color isolation blue', color_blue.filter_image(frame))
+        cv2.imshow('Color isolation green', color_green.filter_image(frame))
+        
+        # Using cv2.putText() method 
+        image = cv2.putText(frame, 'Red: '+str(color_red.get_color_percentage(frame)), (50, 50), cv2.FONT_HERSHEY_SIMPLEX,  
+                        1, (0, 0, 255), 2, cv2.LINE_AA) 
+        image = cv2.putText(image, 'Green: '+str(color_green.get_color_percentage(frame)), (50, 200), cv2.FONT_HERSHEY_SIMPLEX,  
+                        1, (0, 255, 0), 2, cv2.LINE_AA) 
+        image = cv2.putText(image, 'Blue: '+str(color_blue.get_color_percentage(frame)), (50, 400), cv2.FONT_HERSHEY_SIMPLEX,  
+                        1, (255, 0, 0), 2, cv2.LINE_AA) 
+        cv2.imshow('Main', image)
         #print(color_test.get_color_percentage(frame))
         # Check for the 'q' key to exit the loop and terminate the program
         if cv2.waitKey(1) & 0xFF == ord('q'):
